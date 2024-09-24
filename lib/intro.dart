e64d08ceb0ea9a9b2211b97aaa876708dc0e254f
ROCKY <riazulrocky@gmail.com>
2024-09-25 00:13:09 +0600

    quiz app

---
+++ lib/intro.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class intro extends StatefulWidget {
+  const intro({super.key});
+
+  @override
+  State<intro> createState() => _introState();
+}
+
+class _introState extends State<intro> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Container(
+
+        width: double.infinity,
+
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Colors.deepPurpleAccent.shade200,
+            Colors.deepPurple.shade900
+          ],
+
+            begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+
+          )
+        ),
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Image.asset("assets/images/quiz-app.png", width: 200,)
+            // Text(data)
+            // OutlinedButton(onPressed: onPressed, child: child)
+          ],
+        ),
+      ),
+
+    );
+  }
+}
